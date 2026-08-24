43fb3c213c19cb3bde21ee58cb5522f59e025266
tenta11228 <tenta11228@gmail.com>
2026-08-24 03:19:54 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,2 +1,20 @@
-const SITE={name:'NEON MUSIC',telegram:'@YOUR_TELEGRAM_ID'};
-const DATA={songs:[],playlists:[],artists:[],albums:[],categories:[]};
+const SITE={
+    name:'NEON MUSIC',
+    telegram:'@YOUR_TELEGRAM_ID'
+};
+
+const DATA={
+    songs:[
+        {
+            id:'sweet',
+            title:'SWEET',
+            artist:'unkdown',
+            album:'thank u, next',
+            audio:'assets/audio/7rings.mp3'
+        }
+    ],
+    playlists:[],
+    artists:[],
+    albums:[],
+    categories:[]
+};
